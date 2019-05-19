d8cd8d0ccffda8eb87cbda6a7d826f2113c4492e
gracePark9512 <50585429+gracePark9512@users.noreply.github.com>
2019-05-19 14:30:06 +1200

    Build and test

---
+++ build/CMakeFiles/rayTracer.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/AmbientLightSource.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/AmbientLightSource.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/Camera.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/Camera.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/Colour.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/Colour.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/Cube.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/Cube.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/Cylinder.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/Cylinder.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/Direction.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/Direction.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/DirectionalLightSource.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/DirectionalLightSource.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/ImageDisplay.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/ImageDisplay.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/LightSource.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/LightSource.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/Matrix.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/Matrix.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/Normal.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/Normal.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/Object.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/Object.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/PinholeCamera.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/PinholeCamera.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/Plane.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/Plane.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/Point.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/Point.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/PointLightSource.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/PointLightSource.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/Scene.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/Scene.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/SceneReader.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/SceneReader.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/Sphere.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/Sphere.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/SpotLightSource.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/SpotLightSource.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/Transform.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/Transform.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/Vector.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/Vector.cpp.o"
+  "/home/cshome/h/hpark/COSC342/assign02/RayTracer/rayTracerMain.cpp" "/home/cshome/h/hpark/COSC342/assign02/RayTracer/build/CMakeFiles/rayTracer.dir/rayTracerMain.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "cimg_use_png=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/X11/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
